273461b5a727c528840e0b5d8d102f9c9b950160
wuxing55 <81150480+wuxing55@users.noreply.github.com>
2024-05-19 17:37:36 +0800

    Update config.py

--- config.py
+++ config.py
@@ -19,14 +19,14 @@ push plus 微信推送,具体使用参考  https://www.pushplus.plus
 为了安全,这里使用的环境配置.git里面请自行百度如何添加secrets.pycharm也可以自主添加.如果你实在不会,就直接用明文吧（O.o）
 '''
 # PUSH_TOKEN = os.environ.get("PUSHPLUS_KEY")
-PUSH_TOKEN = 'b0731c0031304a5691830b643a654589'
+PUSH_TOKEN = '2e225fba8c4745c0bb7b902dbfc451e4'
 
 '''
 *********** 地图配置 ***********
 获取地点信息,这里用的高德api,需要自己去高德开发者平台申请自己的key
 '''
 # AMAP_KEY = os.environ.get("GAODE_KEY")
-AMAP_KEY = '7251f24064d7f6ef359c279f415c2f3f'
+AMAP_KEY = '77732b468cbd04d5fb4a5426d18e2fd7a'
 
 
 '''
